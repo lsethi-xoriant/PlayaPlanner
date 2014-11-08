fd0f0b35ba4af0927ac7e19a9d144f65c0a8857b
Rachel Hay <rachel.hay@gmail.com>
2014-11-07 20:00:37 -0800

    functional log out link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,21 +10,14 @@ class SessionsController < ApplicationController
 
     if @user
       sign_in(@user)
-      redirect_to user_url(@user)
     else
       flash.now[:errors] = ["Invalid username or password"]
-      render :new
     end
+    redirect_to root_url
   end
 
   def destroy
     sign_out
-    redirect_to new_session_url
+    redirect_to root_url
   end
-
-  # private
-
-  # def user_params
-  #   params.require(:user).permit(:username, :password)
-  # end
 end
